b4206937702a432ab5484d2a698b24a02573d55f
bMitko <mitko3@yahoo.com>
2025-06-27 05:48:02 +0200

    Class 05

--- Homework/Class 05/e-commerce-app/src/App.tsx
+++ Homework/Class 05/e-commerce-app/src/App.tsx
@@ -1,38 +1,10 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-// import { Header } from './components/Header'
-// import { ProductsList } from './pages/Products'
-// import { Home } from './pages/Home'
-// import { ProductsContextProvider } from './context/ProductContext'
-// // import reactLogo from './assets/react.svg'
-// // import viteLogo from '/vite.svg'
-
-// function App() {
-
-//   return (
-//     <div className='global'>
-//     <BrowserRouter>
-//     <ProductsContextProvider>
-//     <Header />
-//     <Routes>
-//       <Route path='/' element={<Home/>} />
-//       {/* <Route path='/products' element={<AllProducts/>} /> */}
-//       <Route path='/products' element={<ProductsList/>} />
-//     </Routes>
-//     </ProductsContextProvider>
-//     </BrowserRouter>
-//     </div>
-//   )
-// }
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProductContextProvider } from "./context/ProductContext";
-// import CategoryPage from "./pages/Home1";
-// import AllProductsPage from "./pages/AllProducts1";
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import HomePage from './pages/Home';
 import AllProductsPage from './pages/AllProducts';
+import './App.css'
 
 function App() {
   return (
